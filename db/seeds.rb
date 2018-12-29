7fa73585e1a04acc826f7dec4465b0531a415e0b
shin1rok <skssikm@gmail.com>
2018-12-29 14:07:16 +0900

    create sample date

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  User.create!(first_name: Faker::Name.first_name,
+               last_name:  Faker::Name.last_name,
+               sex:        Faker::Boolean.boolean,
+               birthday:   Faker::Date.birthday(0, 100),
+               age:        Faker::Number.between(0, 100))
+end
+
+
+100.times do
+  Article.create!(title:        Faker::Book.title,
+                  content:      Faker::Lorem.paragraph,
+                  published_at: Faker::Time.forward(30, :day))
+end
