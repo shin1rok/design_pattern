82e8ce958af96c198e55c02dbc0c27d31631367b
shin1rok <skssikm@gmail.com>
2018-12-29 14:14:31 +0900

    attribute?の使い方が間違っていたので修正

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   def publication_status(article)
-    if article.published?
+    if article.published_at?
       "Published at #{article.published_at.strftime('%A, %B %e')}"
     else
       "Unpublished"
