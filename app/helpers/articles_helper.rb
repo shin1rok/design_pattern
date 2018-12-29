2ce0cc8c39769e497f82ab125778d86d299db616
shin1rok <skssikm@gmail.com>
2018-12-29 13:16:38 +0900

    Decorator適用前

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+  def publication_status(article)
+    if article.published?
+      "Published at #{article.published_at.strftime('%A, %B %e')}"
+    else
+      "Unpublished"
+    end
+  end
 end
